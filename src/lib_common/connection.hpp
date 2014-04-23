a64754f32ee7a2b92618a9a9d2405e685d9ad131
quix <quixver@gmail.com>
2014-04-23 08:44:39 -0400

    Added connection and parsers.

---
+++ src/lib_common/connection.hpp
@@ -0,0 +1,21 @@
+#ifndef __REST_SVC_SERVICE__
+#define __REST_SVC_SERVICE__
+
+
+namespace rest {
+    namespace common {
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+#endif //__REST_SVC_SERVICE__
---
+++ src/rest_svc/service.hpp
@@ -0,0 +1,23 @@
+#ifndef __REST_SVC_SERVICE__
+#define __REST_SVC_SERVICE__
+
+
+#include <boost/noncopyable.hpp>
+
+
+namespace rest {
+    namespace common {
+
+
+        class service : private boost::noncopyable
+        {
+
+
+
+        };
+
+
+    }
+}
+
+#endif //__REST_SVC_SERVICE__
